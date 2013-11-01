f9039be5348870d738ba9b357905ad3fdcec6a9b
David E. Narváez <david.narvaez@computer.org>
2013-11-01 13:58:22 -0400

    Basic experimental filling of node information

--- src/artgraph/plugins/infobox.py
+++ src/artgraph/plugins/infobox.py
@@ -16,6 +16,16 @@ class InfoboxPlugin(Plugin):
 
             for t in templates:
                 if t.name.matches('Infobox musical artist'):
+                    # Fill in current node info
+                    if t.has('birth_name'):
+                        name = str(t.get('birth_name').value)
+                    
+                        db = self.get_artistgraph_connection()
+                        cursor = db.cursor()
+                        cursor.execute("UPDATE artist SET fName = %s WHERE artistID = %s", (name, self._node.get_id()))
+                        db.commit()
+                        db.close()
+                    
                     associated_acts = t.get('associated_acts')
                 
                     for w in associated_acts.value.filter_wikilinks():
