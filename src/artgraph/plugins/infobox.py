c1da16839a44e7fbcaf2f287c8da761bae279f22
David E. Narváez <david.narvaez@computer.org>
2013-11-09 01:17:14 -0500

    Small formatting fixes and optimizations

--- src/artgraph/plugins/infobox.py
+++ src/artgraph/plugins/infobox.py
@@ -17,9 +17,7 @@ class ArtistInfoboxPlugin(Plugin):
         wikicode = self.get_wikicode(self._node.get_dbtitle())
         
         if wikicode:
-            templates = wikicode.filter_templates()
-
-            for t in templates:
+            for t in wikicode.filter_templates():
                 if t.name.matches('Infobox musical artist'):
                     # Fill in current node info
                     if t.has('birth_name'):
@@ -42,33 +40,35 @@ class ArtistInfoboxPlugin(Plugin):
                          
                         for w in genres.value.filter_wikilinks():
                             relationships.append(ArtistGenreRelationship(self._node, Node(str(w.title), NodeTypes.GENRE)))
+                            
+                    break
                 
         return relationships
 
 class GenreInfoboxPlugin(Plugin):
     def __init__(self, node):
         Plugin.__init__(self, node)
-    
+      
     @staticmethod
     def get_target_node_type():
         return NodeTypes.GENRE
-    
+      
     def get_nodes(self):
         from artgraph.node import Node, NodeTypes
         from artgraph.relationship import SubgenreRelationship
         
         relationships = []
         wikicode = self.get_wikicode(self._node.get_dbtitle())
-        
+          
         if wikicode:
-            templates = wikicode.filter_templates()
-
-            for t in templates:
+            for t in wikicode.filter_templates():
                 if t.name.matches('Infobox music genre'):
                     if t.has('subgenres'):
                         genres = t.get('subgenres')
-                         
+                           
                         for w in genres.value.filter_wikilinks():
                             relationships.append(SubgenreRelationship(self._node, Node(str(w.title), NodeTypes.GENRE)))
-                
+                              
+                    break
+                  
         return relationships
